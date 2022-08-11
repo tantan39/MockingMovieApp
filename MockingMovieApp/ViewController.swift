b7a54630abf043891570f873f9b6eaebdc012ce1
Tan Tran <tantt.dev@gmail.com>
2022-08-10 20:10:24 -0400

    Initial Commit

---
+++ MockingMovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MockingMovieApp
+//
+//  Created by Tan Tan on 8/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
