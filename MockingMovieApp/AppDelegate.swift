6f114441e234acbf9ddd974d6a304abc15c99f1f
Tan Tran <tantan992@gmail.com>
2023-02-09 10:27:17 -0500

    Remove master log

--- MockingMovieApp/AppDelegate.swift
+++ MockingMovieApp/AppDelegate.swift
@@ -9,9 +9,6 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    // Add log on master
-    // Add log dev
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
